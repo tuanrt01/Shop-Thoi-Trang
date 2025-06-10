069871c5f072fd9a9656996e07ee86ce944d4897
lolitoichoi <bestyasuohoatien@gmail.com>
2025-06-10 16:51:39 +0700

    first commit

---
+++ shopthoitrang1/shopthoitrang/db/connect.php
@@ -0,0 +1,8 @@
+<?php
+$con = mysqli_connect("localhost","root","","shopthoitrang1");
+
+// Check connection
+if (mysqli_connect_errno()) {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+?>
